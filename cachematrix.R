6bc0afe8314f6de7e72dba517154e8b1fa8ab71e
James Townend <jamestownend@gmail.com>
2017-07-19 21:04:28 +0200

    Solves matrix or uses precached solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to create and solve a matrix object, checking first if there is a
+## cached inverse already and using that if so
 
-## Write a short comment describing this function
+## Creates the matrix object capable of caching its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## Solves a given matrix (already created in makeCacheMatrix) or uses
+## the cached inverse if it has already been solved
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    i <- x$getinverse()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
 }
